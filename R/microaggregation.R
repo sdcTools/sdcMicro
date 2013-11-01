ea4cea7a648ab756478f32aa45086a0899a804f0
Bernhard <bmeindl1981@gmail.com>
2013-11-01 15:53:38 +0100

    fix microaggregation(): do not add strata_variables to manipNumVar-slot

--- R/microaggregation.R
+++ R/microaggregation.R
@@ -21,7 +21,7 @@ setMethod(f='microaggregation', signature=c('sdcMicroObj'),
       obj <- nextSdcObj(obj)
       x[,variables] <- res$mx
       
-      obj <- set.sdcMicroObj(obj, type="manipNumVars", input=list(as.data.frame(x)))
+      obj <- set.sdcMicroObj(obj, type="manipNumVars", input=list(as.data.frame(x[,variables])))
       
       obj <- dRisk(obj)
 #      obj <- dRiskRMD(obj)
