8d49dfd93a20d03f4ada085eac3c951b907edd82
alexkowa <alex@kowarik.net>
2013-11-28 08:10:09 +0100

    Adding some tests should help keep the package stable!

---
+++ tests/microaggregation_test.R
@@ -0,0 +1,13 @@
+require(sdcMicro)
+### for a data.frame
+dat <- data.frame(x=rnorm(100))
+microaggregation(obj=dat, variables="x", method="mdav", aggr=3, measure="mean")
+dat <- data.frame(x=rnorm(100), y=rnorm(100))
+microaggregation(obj=dat, variables="x", method="mdav", aggr=3, measure="mean")
+microaggregation(obj=dat, variables=c("x","y"), method="mdav", aggr=3, measure="mean")
+# for a sdcObj
+set.seed( 199723 )
+activedataset <- testdata
+sdcObject <- createSdcObj(activedataset,keyVars=c('urbrur','roof','sex','age'),numVars=c('expend','income','savings'),weightVar=c('sampling_weight'),hhId=c('ori_hid'))
+sdcObject <- microaggregation(sdcObject,aggr=c( 3 ), method=c('mdav'), variables=c('expend'),strata_variables=c('sex'))
+
---
+++ tests/reporting_test.R
@@ -0,0 +1,18 @@
+require(sdcMicro)
+set.seed( 199723 )
+activedataset <- testdata
+sdcObject <- createSdcObj(activedataset,keyVars=c('urbrur','roof','sex','age'),numVars=c('expend','income','savings'),weightVar=c('sampling_weight'),hhId=c('ori_hid'))
+sdcObject <- varToFactor(sdcObject,var=c('urbrur'))
+sdcObject <- varToFactor(sdcObject,var=c('roof'))
+sdcObject <- varToFactor(sdcObject,var=c('sex'))
+sdcObject <- renameVars(sdcObject,var=c('sex'), before=c('1'), after=c('m'))
+sdcObject <- renameVars(sdcObject,var=c('sex'), before=c('2'), after=c('w'))
+sdcObject <- globalRecode(sdcObject,column=c('age'), breaks=c( 6 ), labels=NULL)
+sdcObject <- groupVars(sdcObject,var=c('age'), before=c('(63.365,79.23]','(79.23,95.095]'), after=c('(63.365,95.095]'))
+sdcObject <- localSuppression(sdcObject,k=c( 2 ),importance=c( 1 , 3 , 1 , 3 ))
+sdcObject <- localSuppression(sdcObject,k=c( 3 ),importance=c( 1 , 3 , 1 , 3 ))
+sdcObject <- microaggregation(sdcObject,aggr=c( 3 ), method=c('mdav'), variables=c('expend'),strata_variables=c('sex'))
+
+td  <- tempdir()
+report(sdcObject,outdir=td)
+report(sdcObject,outdir=td,internal=TRUE,filename="fullreport")
