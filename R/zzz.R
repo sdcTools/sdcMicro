6d133cd295ce0099c722ae50fb3edffeaf8572dc
alexkowa <alex@kowarik.net>
2013-12-09 07:25:50 +0100

    Added the link a new line in the package startup message 'Suggestions and bug-reports can be submitted at: https://github.com/alexkowa/sdcMicro/issues'

--- R/zzz.R
+++ R/zzz.R
@@ -4,7 +4,8 @@
 	packageStartupMessage("for references have a look at\n")
 	packageStartupMessage("citation('sdcMicro')    \n")                 
 	packageStartupMessage("\n Note that since version 2.6.6. the graphical user-interface is provided\n by package sdcMicroGUI.\n")
-	packageStartupMessage("--------\n\n")  
+	packageStartupMessage("--------\n\n")
+  packageStartupMessage("Suggestions and bug-reports can be submitted at: https://github.com/alexkowa/sdcMicro/issues")
    
 }
 
