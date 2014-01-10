a392494e24c7e7f41a66431cb93c4983f2e5f055
matthias99 <matthias.templ@gmail.com>
2014-01-10 12:25:50 +0100

    f1 was NULL - changed

--- tests/globalRecoding_test.R
+++ tests/globalRecoding_test.R
@@ -9,6 +9,6 @@ sdc2 <- globalRecode(sdc, column="age",
 		breaks=c(1,9,19,29,39,49,59,69,100), labels=1:8)
 print(sdc2)
 
-f1 <- get.sdcMicroObj(sdc, "risk")$fk
-f2 <- get.sdcMicroObj(sdc2, "risk")$fk
+f1 <- get.sdcMicroObj(sdc, "risk")$individual[,"fk"]
+f2 <- get.sdcMicroObj(sdc2, "risk")$individual[,"fk"]
 if(identical(f1,f2)) stop("frequency counts are not updated")
