97c72ddc437d5d84b8636bcfe77307a5d841e404
bernhard <bernhard.meindl@statistik.gv.at>
2014-03-26 14:49:07 +0100

    fix bug in localSupp(); still needs more attention

--- R/localSupp.R
+++ R/localSupp.R
@@ -7,23 +7,24 @@ setMethod(f='localSupp', signature=c('sdcMicroObj'),
       TF <- rk>threshold
       if(!all(keyVar%in%colnames(manipData)))
         stop("keyVar must be a defined categorical keyVariable!\n")
-      manipData[TF,keyVar] <- NA
-      obj <- nextSdcObj(obj)
-      obj <- set.sdcMicroObj(obj, type="manipKeyVars", input=list(manipData))
-      a <- get.sdcMicroObj(obj, type="origData")[,get.sdcMicroObj(obj, type="keyVars")]
-      b <- manipData
-      w = which(is.na(a))
-      r = w%%nrow(a)
-      cc = ceiling(w/nrow(a))
-      wb = which(is.na(b))
-      rb = wb%%nrow(b)
-      cb = ceiling(wb/nrow(b))
-      d = data.frame(id=1:ncol(a), before=NA, after=NA)
-      d[,2:3] <- t(sapply(1:ncol(a), function (x) c(sum(cc==x), sum(cb==x))))
-      obj <- set.sdcMicroObj(obj, type="localSuppression", input=list(list(d$after-d$before)))
-      
-      obj <- calcRisks(obj)
-      
+
+      if ( any(TF) ) {
+        manipData[which(TF),keyVar] <- NA
+        obj <- nextSdcObj(obj)
+        obj <- set.sdcMicroObj(obj, type="manipKeyVars", input=list(manipData))
+        a <- get.sdcMicroObj(obj, type="origData")[,get.sdcMicroObj(obj, type="keyVars")]
+        b <- manipData
+        w = which(is.na(a))
+        r = w%%nrow(a)
+        cc = ceiling(w/nrow(a))
+        wb = which(is.na(b))
+        rb = wb%%nrow(b)
+        cb = ceiling(wb/nrow(b))
+        d = data.frame(id=1:ncol(a), before=NA, after=NA)
+        d[,2:3] <- t(sapply(1:ncol(a), function (x) c(sum(cc==x), sum(cb==x))))
+        obj <- set.sdcMicroObj(obj, type="localSuppression", input=list(list(d$after-d$before)))
+        obj <- calcRisks(obj)
+      }
       obj
     })
 setMethod(f='localSupp', signature=c("ANY"),
