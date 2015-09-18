456a67caa6d40b343005d70f5d652329976cf7d4
bernhard <bernhard.meindl@statistik.gv.at>
2015-09-18 11:19:04 +0200

    + add roxygen doku

--- R/topBotCoding.R
+++ R/topBotCoding.R
@@ -1,3 +1,39 @@
+#' Top and Bottom Coding
+#'
+#' Function for Top and Bottom Coding.
+#'
+#' Extreme values are replaced by one value to reduce the disclosure risk.
+#'
+#' @name topBotCoding
+#' @aliases topBotCoding-methods topBotCoding,ANY-method
+#' topBotCoding,sdcMicroObj-method topBotCoding
+#' @docType methods
+#' @param obj vector or one-dimensional matrix or data.frame or object of class
+#' \code{\link{sdcMicroObj-class}}
+#' @param value limit, from where it should be top- or bottom-coded
+#' @param replacement replacement value.
+#' @param kind top or bottom
+#' @param column xxx
+#' @return Top or bottom coded data or modified \code{\link{sdcMicroObj-class}}.
+#' @section Methods: \describe{
+#' \item{list("signature(obj = \"ANY\")")}{}
+#' \item{list("signature(obj = \"sdcMicroObj\")")}{}}
+#' @author Matthias Templ
+#' @seealso \code{\link{indivRisk}}
+#' @keywords manip
+#' @export
+#' @examples
+#'
+#' data(free1)
+#' topBotCoding(free1[,"DEBTS"], value=9000, replacement=9100, kind="top")
+#'
+#' ## for objects of class sdcMicro:
+#' data(testdata2)
+#' sdc <- createSdcObj(testdata2, keyVars=c('urbrur','roof','walls','water','electcon','relat','sex'),
+#'            numVars=c('expend','income','savings'), w='sampling_weight')
+#' sdc <- topBotCoding(sdc, value=500000, replacement=1000, column="income")
+#' testdataout <- extractManipData(sdc)
+#'
 setGeneric('topBotCoding', function(obj, value, replacement, kind="top", column=NULL) {standardGeneric('topBotCoding')})
 setMethod(f='topBotCoding', signature=c('sdcMicroObj'),
 definition=function(obj, value, replacement, kind="top", column=NULL) {
