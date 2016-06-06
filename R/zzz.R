3076a4e0b7d05e8032d53c88f976f4a0a2be7cce
bernhard <bernhard.meindl@data-analysis.at>
2016-06-06 22:00:58 +0200

    update startup-msg

--- R/zzz.R
+++ R/zzz.R
@@ -2,7 +2,7 @@
   msg <- "--------\n"
   msg <- paste0(msg, "This is sdcMicro v",utils::packageVersion("sdcMicro"),".\n")
   msg <- paste0(msg, "For references, please have a look at citation('sdcMicro')\n")
-  msg <- paste0(msg, "Note: since version 2.6.6, the graphical user-interface is provided by package sdcMicroGUI.\n")
+  msg <- paste0(msg, "Note: since version 4.7.0, the graphical user-interface is a shiny-app that can be started with sdcGUI().\n")
   msg <- paste0(msg, "Please submit suggestions and bugs at: https://github.com/alexkowa/sdcMicro/issues\n")
   msg <- paste0(msg,"--------")
   packageStartupMessage(msg)
