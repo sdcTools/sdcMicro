4d2beb7ea60ca52d04e261f46c5853804f42b7f7
bernhard <bernhard.meindl@data-analysis.at>
2016-07-19 09:32:08 +0200

    + some more explanation, fixes #85

--- inst/shiny-examples/sdcGUI/controllers/ui_undo.R
+++ inst/shiny-examples/sdcGUI/controllers/ui_undo.R
@@ -2,6 +2,7 @@ output$ui_undo <- renderUI({
   btn_undo <- myActionButton("btn_undo", "Undo last Step", "danger")
   list(
     htmlTemplate("tpl_one_col.html", inp=h2("Undo last step")),
+    htmlTemplate("tpl_one_col.html", inp=p("Clicking the button below will remove (if possible) the last anonymization step!")),
     htmlTemplate("tpl_one_col.html", inp=btn_undo)
   )
 })
