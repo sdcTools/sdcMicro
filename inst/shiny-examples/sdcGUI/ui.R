c548d9b9b0d5148cc105f115a2fa1457539a1cf3
bernhard <bernhard.meindl@data-analysis.at>
2016-09-24 22:59:01 +0200

    + renamed tab 'View/Analyse Results' to 'Risk/Utility'

--- inst/shiny-examples/sdcGUI/ui.R
+++ inst/shiny-examples/sdcGUI/ui.R
@@ -3,7 +3,7 @@ shinyUI(navbarPage(id="mainnav", theme="bootswatch_yeti.css","sdcMicro GUI",
   tabPanel("About/Help", uiOutput("ui_about")),
   tabPanel("Microdata", uiOutput("ui_inputdata")),
   tabPanel("Anonymize", uiOutput("ui_anonymize")),
-  tabPanel("View/Analyse Results", uiOutput("ui_results")),
+  tabPanel("Risk/Utility", uiOutput("ui_results")),
   tabPanel("Export Data", uiOutput("ui_export")),
   tabPanel("Reproducibility", uiOutput("ui_script")),
   tabPanel("Undo", uiOutput("ui_undo"))
