e52a78b337ce5e3e1ff9e4ad7c20ee0c7caff37a
bernhard <bernhard.meindl@data-analysis.at>
2016-10-05 13:14:30 +0200

    + improve startup time on first load?

--- inst/shiny-examples/sdcGUI/controllers/ui_about.R
+++ inst/shiny-examples/sdcGUI/controllers/ui_about.R
@@ -1,16 +1,10 @@
 output$ui_about <- renderUI({
-  if (values$starting) {
-    return(NULL)
-  }
-  txt <- list("Some information about",code("sdcMicro"),"and how to use this
-    graphical interface.")
-
   fluidRow(
     column(12, h4("About the Interface", align="center")),
-    column(12, p(txt, align="center")),
-    column(12, h4("Help", align="center")),
-    column(12, p("Link to GUI-Tutorial", align="center")),
-    column(12, p("Link to GUI-Tutorial", align="center")),
-    column(12, p("If you already have a Problem that was exported from the GUI, you can upload it in Tab",code("Reproducibility"),".", align="center"))
+    column(12, p("Some information about",code("sdcMicro"),"and how to use this graphical interface."), align="center"),
+    column(12, h4("Help"), align="center"),
+    column(12, p("Link to GUI-Tutorial"), align="center"),
+    column(12, p("Link to GUI-Tutorial"), align="center"),
+    column(12, p("If you already have a Problem that was exported from the GUI, you can upload it in Tab",code("Reproducibility"),"."), align="center")
   )
 })
