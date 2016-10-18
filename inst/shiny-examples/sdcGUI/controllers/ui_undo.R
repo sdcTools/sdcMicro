1275001d379ff639660e5beffed9a68f1f8f099b
bernhard <bernhard.meindl@gmail.com>
2016-10-18 06:50:18 +0200

    + cosmetics

--- inst/shiny-examples/sdcGUI/controllers/ui_undo.R
+++ inst/shiny-examples/sdcGUI/controllers/ui_undo.R
@@ -1,18 +1,18 @@
 output$ui_undo <- renderUI({
-  if (is.null(sdcObj())) {
+  curObj <- sdcObj()
+  if (is.null(curObj)) {
     return(noSdcProblem(uri="ui_undo"))
   }
-  if (is.null(sdcObj()@prev)) {
+  if (is.null(curObj@prev)) {
     return(fluidRow(
-      column(12, h4("Undo last step", align="center")),
-      column(12, p("Currently, no step can be undone.", align="center"))))
+      column(12, h4("Undo last step"), align="center"),
+      column(12, p("Currently, no step can be undone."), align="center")))
   } else {
     btn_undo <- myActionButton("btn_undo", "Undo last Step", "danger", css.class="btn-xs")
-
     return(fluidRow(
-      column(12, h4("Undo last step", align="center")),
-      column(12, p("Clicking the button below will remove (if possible) the following anonymization step!", align="center")),
+      column(12, h4("Undo last step"), align="center"),
+      column(12, p("Clicking the button below will remove (if possible) the following anonymization step!"), align="center"),
       column(12, list(code(obj$lastaction), br(), br()), align="center"),
-      column(12, p(btn_undo, align="center"))))
+      column(12, btn_undo, align="center")))
   }
 })
