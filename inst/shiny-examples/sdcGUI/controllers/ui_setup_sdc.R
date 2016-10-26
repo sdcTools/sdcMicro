bd972cb053de9242bfbf4b5cd5cf244ec57853fd
thijsbenschop <thijsbenschop@gmail.com>
2016-10-26 15:04:25 +0200

    Correct capitalization

--- inst/shiny-examples/sdcGUI/controllers/ui_setup_sdc.R
+++ inst/shiny-examples/sdcGUI/controllers/ui_setup_sdc.R
@@ -109,8 +109,8 @@ output$ui_sdcObj_summary <- renderUI({
     riskyobs <- x[[1]]$riskyObs
     out <- fluidRow(
       column(12, h4("Risk-measures for categorical variables"), align="center"),
-      column(12, p("We expect",code(as.integer(reident$mod)),"(",code(paste0(reident$mod_p,"%")),") Re-identifications in the population. In the original
-        data, we expected to have",code(as.integer(reident$orig)),"(",code(paste0(reident$orig_p,"%")),") Re-identifications."), align="center"),
+      column(12, p("We expect",code(as.integer(reident$mod)),"(",code(paste0(reident$mod_p,"%")),") re-identifications in the population. In the original
+        data, we expected to have",code(as.integer(reident$orig)),"(",code(paste0(reident$orig_p,"%")),") re-identifications."), align="center"),
       column(12, p("Currently there are",code(as.integer(riskyobs$mod)),"observations that have a higher risk that the main part of the data. In the
         original data this number was",code(as.integer(riskyobs$orig)),"."), align="center"))
     out
