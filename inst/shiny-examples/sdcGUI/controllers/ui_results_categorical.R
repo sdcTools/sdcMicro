1622db53c72646d5f28277bb3f0403bcaabef1fe
bernhard <bernhard.meindl@data-analysis.at>
2016-11-10 21:06:30 +0100

    + show only 3 digits in violating k-anon table + update vignette on sdcGUI()

--- inst/shiny-examples/sdcGUI/controllers/ui_results_categorical.R
+++ inst/shiny-examples/sdcGUI/controllers/ui_results_categorical.R
@@ -276,6 +276,7 @@ output$ui_rescat_violating_kanon <- renderUI({
       return(NULL)
     }
     df <- cbind(get_origData()[ii, get_keyVars()], risks[ii,])
+    df$risk <- formatC(df$risk, format="f", digits=3)
     df[order(df$fk),]
   })
 
