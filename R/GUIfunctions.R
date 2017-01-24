ff0c95a79c5dec520c793580da925f4fb70358d0
thijsbenschop <thijsbenschop@gmail.com>
2017-01-24 11:34:27 +0100

    Empty string to no label

--- R/GUIfunctions.R
+++ R/GUIfunctions.R
@@ -71,7 +71,7 @@ extractLabels <- function(dat){
     colnames(varLab) <- c("var.name", "var.label")
     rownames(varLab) <- NULL
     # Set to NULL values in var.label to NA
-    varLab[which(sapply(sapply(dat, function(x) { attr(x, "label") }), is.null)), 2] <- ""
+    varLab[which(sapply(sapply(dat, function(x) { attr(x, "label") }), is.null)), 2] <- "No label"
   } else {
     varLab <- NULL
   }
