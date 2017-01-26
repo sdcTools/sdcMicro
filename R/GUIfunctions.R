241bad17cd7beb5799dab3137cc4d929272df76b
thijsbenschop <thijsbenschop@gmail.com>
2017-01-26 09:49:04 +0100

    Add stop

--- R/GUIfunctions.R
+++ R/GUIfunctions.R
@@ -400,8 +400,8 @@ readMicrodata <- function(path, type, convertCharToFac=TRUE, drop_all_missings=T
     res <- tryCatchFn(read_dta(file=path))
     lab <- extractLabels(res)
     if("integer" %in% class(lab)){
-      res <- tryCatchFn(read_dta(file="variable labels aren't UTF-8"))
-      res$message <- paste0(res$message, "\n", lab, "variable labels are not UTF-8 encoded. Correct the labels and reload the data.")
+      res <- try(read_dta(file="variable labels aren't UTF-8"))
+      res$message <- paste0(res$message, "\n ", lab, "variable labels are not UTF-8 encoded. Correct the labels and reload the data.")
       return(res)
     }
   }
