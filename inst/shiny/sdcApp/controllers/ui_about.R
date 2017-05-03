cffbb9f287308f06a94b82195868d45c06c95230
Evan Karageorgos <vangelis.karageorgos@gmail.com>
2017-05-03 11:17:27 +0100

    (WIP) Proposal 1 (inspired by http://data.worldbank.org/ )

--- inst/shiny/sdcApp/controllers/ui_about.R
+++ inst/shiny/sdcApp/controllers/ui_about.R
@@ -58,9 +58,9 @@ observeEvent(input$stop_sdcApp,{
 #
 
 output$ui_about <- renderUI({
-  btn_credits <- bsButton("btn_credits", "here", style="primary", size="extra-small")
+  btn_credits <- bsButton("btn_credits", "here", style="primary", size="extra-small", class="wb-btn-inline")
   out <- fluidRow(
-    column(width = 8, offset = 2, h3(strong("sdcApp"))),
+    column(width = 8, offset = 2, h3(("sdcApp"))),
     column(width = 8, offset = 2, p("This graphical user interface of",code("sdcMicro")," allows you to anonymize microdata even if you are not an
       expert in the",code("R"),"programming language. Detailed information on how to use this graphical user-interface (GUI) can be found in a tutorial (a so-called vignette) that is included in the",code("sdcMicro"),"package.
     The vignette is available from the",tags$a("CRAN", href="https://cran.r-project.org/web/packages/sdcMicro/vignettes/sdcApp.html", target="_blank"), "website or by typing",code('vignette("sdcApp", package="sdcMicro")'),"into your",code("R"),"prompt.")),
@@ -69,38 +69,38 @@ output$ui_about <- renderUI({
   )
 
   if (is.null(inputdata())) {
-    btn <- bsButton(paste0("btn_a_micro_ui_about"), label=("this button"), style="primary", size="extra-small")
-    btn_pi <- bsButton(paste0("nodata_script_about"), label=("here"), style="primary", size="extra-small")
+    btn <- bsButton(paste0("btn_a_micro_ui_about"), label=("this button"), style="primary", size="extra-small", class="wb-btn-inline")
+    btn_pi <- bsButton(paste0("nodata_script_about"), label=("here"), style="primary", size="extra-small", class="wb-btn-inline")
 
     txt_start <- paste0("To get started, you need to upload a file with microdata to the GUI. You can do so by clicking ",btn,". ")
     txt_start <- paste0(txt_start, "Alternatively, you can upload a previously saved problem instance by clicking ",btn_pi,".")
     out <- list(out, fluidRow(
-      column(width = 8, offset = 2, h4(strong("Getting started"))),
+      column(width = 8, offset = 2, h4(("Getting started"))),
       column(width = 8, offset = 2, p(HTML(txt_start)))
     ))
   }
 
-  pp <- textInput("path_export_data", label=h5("Enter a directory where any exported files (data, script, problem instances) should be saved to"),
-    placeholder=paste("e.g:", obj$path_export), width="50%")
-  stop_btn <- bsButton("stop_sdcApp", label="Stop the GUI", style="primary", size="extra-small", onclick="setTimeout(function(){window.close();},500);")
+  pp <- textInput("path_export_data", label=p("Enter a directory where any exported files (data, script, problem instances) should be saved to"),
+    placeholder=paste("e.g:", obj$path_export), width="80%")
+  stop_btn <- bsButton("stop_sdcApp", label="Stop the GUI", style="primary", size="extra-small", class="wb-btn-inline", onclick="setTimeout(function(){window.close();},500);")
   out <- list(out, fluidRow(
-    column(width = 8, offset = 2, h4(strong("Set storage path"))),
+    column(width = 8, offset = 2, h4(("Set storage path"))),
     column(width = 8, offset = 2, p("Currently, all output, such as anonymized data, scripts and reports, will be saved to",code(obj$path_export),".")),
     column(width = 8, offset = 2, p("You can change the default path, where all output from the GUI will be saved. You can change this path any time
       later as well by returing to this tab.")),column(width = 8, offset = 2, pp),
     column(width = 8, offset = 2, uiOutput("btn_update_export_path")),
-    column(width = 8, offset = 2, h4(strong("Stop the interface"))),
+    column(width = 8, offset = 2, h4(("Stop the interface"))),
     column(width = 8, offset = 2, p("By clicking ", stop_btn,", you can stop the graphical user interface at any time during the anonymization process.
       If you have started the interface as",code('x <- sdcApp()'),",",code("x"),
       "will contain the micro data and the sdc problem at the state just before stopping the GUI.")),
-    column(width = 8, offset = 2, h4(strong("Restart the interface"))),
+    column(width = 8, offset = 2, h4(("Restart the interface"))),
     column(width = 8, offset = 2, p("Should you accidentally close this browser window, you can open your current problem instance by entering the local IP address
       specified in the",code("R"),"console in which you started the GUI. The address starts with", code("http://127.0.0.1:"), ". You
       can also use the refresh button of your browser in case the GUI freezes."))
   ))
 
   out <- list(out, fluidRow(
-    column(width = 8, offset = 2, h4(strong("Contact and Feedback"))),
+    column(width = 8, offset = 2, h4(("Contact and Feedback"))),
     column(width = 8, offset = 2, p("In case you have any suggestions or bug reports, please file an issue at the",
       tags$a("issue tracker", href="https://www.github.com/alexkowa/sdcMicro/issues", target="_blank"),"in our",
       tags$a("GitHub repo", href="https://www.github.com/alexkowa/sdcMicro", target="_blank"),".")),
