26e6d1e8d060ab9ac9749c03fa695dfd0a44801b
bernhard <bernhard.meindl@gmail.com>
2017-06-25 15:27:30 +0200

    dealing with a problem if inputs are tbl_dfs, fixing #223

--- R/dUtility.R
+++ R/dUtility.R
@@ -87,7 +87,7 @@ dUtilityWORK <- function(x, xm, method = "IL1") {
   if (method == "IL1") {
     a <- x
     for (i in 1:dim(x)[2]) {
-      a[, i] <- abs((x[, i] - xm[, i])/sd(x[, i], na.rm = TRUE) * sqrt(2))
+      a[[i]] <- abs((x[[i]] - xm[[i]])/sd(x[[i]], na.rm = TRUE) * sqrt(2))
     }
     infLoss1 <- 1/(dim(x)[2] * dim(x)[1]) * sum(a, na.rm = TRUE)
     return(infLoss1)
