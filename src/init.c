a151bfa95622dddda8364e12e7c63cb0354b1161
thijsbenschop <thijsbenschop@gmail.com>
2017-08-16 09:36:17 +0200

    Alignment init.c

--- src/init.c
+++ src/init.c
@@ -4,7 +4,7 @@
 #include <R_ext/Rdynload.h>
 
 /* FIXME:
-   Check these declarations against the C/Fortran source code.
+Check these declarations against the C/Fortran source code.
 */
 
 /* .Call calls */
@@ -18,19 +18,19 @@ extern SEXP measure_threshold(SEXP, SEXP);
 extern SEXP _sdcMicro_cpp_calcSuppInds(SEXP, SEXP, SEXP);
 
 static const R_CallMethodDef CallEntries[] = {
-    {"LocalRecProg_cpp",          (DL_FUNC) &LocalRecProg_cpp,          9},
-    {"Mdav",                      (DL_FUNC) &Mdav,                      5},
-    {"RankSwap",                  (DL_FUNC) &RankSwap,                  9},
-    {"Suda2",                     (DL_FUNC) &Suda2,                     5},
-    {"measure_hierachical",       (DL_FUNC) &measure_hierachical,       1},
-    {"measure_risk_cpp",          (DL_FUNC) &measure_risk_cpp,          6},
-    {"measure_threshold",         (DL_FUNC) &measure_threshold,         2},
-    {"_sdcMicro_cpp_calcSuppInds", (DL_FUNC) &_sdcMicro_cpp_calcSuppInds, 3},
-    {NULL, NULL, 0}
+  {"LocalRecProg_cpp",          (DL_FUNC) &LocalRecProg_cpp,          9},
+  {"Mdav",                      (DL_FUNC) &Mdav,                      5},
+  {"RankSwap",                  (DL_FUNC) &RankSwap,                  9},
+  {"Suda2",                     (DL_FUNC) &Suda2,                     5},
+  {"measure_hierachical",       (DL_FUNC) &measure_hierachical,       1},
+  {"measure_risk_cpp",          (DL_FUNC) &measure_risk_cpp,          6},
+  {"measure_threshold",         (DL_FUNC) &measure_threshold,         2},
+  {"_sdcMicro_cpp_calcSuppInds", (DL_FUNC) &_sdcMicro_cpp_calcSuppInds, 3},
+  {NULL, NULL, 0}
 };
 
 void R_init_sdcMicro(DllInfo *dll)
 {
-    R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
-    R_useDynamicSymbols(dll, FALSE);
+  R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
+  R_useDynamicSymbols(dll, FALSE);
 }
