4f66186bca57aa59a5fd28d0f51473d13450877b
templ@statistik.tuwien.ac.at <templ@statistik.tuwien.ac.at>
2018-12-20 13:32:06 +0100

    mvTopCoding

---
+++ R/mvTopCoding.R
@@ -0,0 +1,44 @@
+#' @name mvTopCoding
+#' @title Detection and winsorization of multivariate outliers
+#' @rdname mvTopCoding
+#' @usage mvTopCoding(x, maha=NULL,center=NULL,cov=NULL, alpha=0.025)
+#' @author Johannes Gussenbauer, Matthias Templ
+#' @description Imputation and detection of outliers 
+#' @details Winsorizes the potential outliers on the ellipsoid defined by MD2 in direction to the center of the data
+#' @param x data frame or matrix or table
+#' @param maha squared mahalanobis distance of each observation
+#' @param center center of data, needed for calcualtion of mahalanobis distance (if not provide)
+#' @param sigma covariance matrix of data, needed for calcualtion of mahalanobis distance (if not provide)
+#' @param alpha significance level, determining the ellipsoide to which outliers should be placed upon
+#' @return the imputed/winsorized data
+#' @importFrom robustbase covMcd
+#' @export 
+#' @examples 
+#' set.seed(123)
+#' x <- MASS::mvrnorm(20, mu = c(5,5), Sigma = matrix(c(1,0.9,0.9,1), ncol = 2))
+#' x[1,1] <- 3
+#' x[1,2] <- 6
+#' plot(x)
+#' ximp <- mvTopCoding(x)
+mvTopCoding <- function(x, maha=NULL,center=NULL,
+                           cov=NULL, alpha=0.025){
+  stopifnot(is.numeric(x) || is.logical(x), is.atomic(x))
+ 
+  if(!is.data.table(x))x <- as.data.table(x)
+  
+  p <- ncol(x)
+  d <- qchisq(1-alpha,df=p)
+  
+  if(is.null(maha)){
+    cv <- robustbase::covMcd(x=x)
+    center <- cv$center
+    maha <- mahalanobis(x=x, center=center,cov=cv$cov)
+  }
+  
+  index <- which(maha > d)
+  a <- as.matrix(sqrt(d/maha[index]))
+  
+  x[index] <- x[index]*a+(1-a)%*%t(unlist(center))
+  
+  return(x)
+}
