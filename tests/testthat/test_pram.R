a9bcb47d20405e763d387a32f2bca9c39142958a
alexkowa <alex@kowarik.net>
2019-07-29 12:21:56 +0200

    pram checks

--- tests/testthat/test_pram.R
+++ tests/testthat/test_pram.R
@@ -21,8 +21,6 @@ test_that("pram on a data.frame", {
   ## variables "urbrur" and "sex"
   res <- pram(testdata, variables="roof",
               strata_variables=c("urbrur","sex"))
-  print(res)
-  summary(res)
   expect_false(identical(res$roof,res$roof_pram))
 })
 
@@ -42,16 +40,14 @@ test_that("pram on a data.frame 2", {
   ## default parameters (pd=0.8 and alpha=0.5) for the generation
   ## of the invariant transition matrix will be used for all variables
   res1 <- pram(testdata, variables=c("roof","walls","water"))
-  print(res1)
+  expect_false(all(res1$roof_pram==testdata$roof&res1$walls_pram==testdata$walls&res1$water_pram==testdata$water))
 })
+
 test_that("pram on a data.frame, non default pd", {
   ## specific parameters for each variable
-  res2 <- pram(testdata,variables=c("roof","walls","water"),
+  res1 <- pram(testdata,variables=c("roof","walls","water"),
                pd=c(0.95,0.8,0.9), alpha=0.5)
-  print(res2)
-  ## detailed information on pram-parameters (such as the transition matrix 'Rs')
-  ## is stored in the output, eg. for variable 'roof'
-  attr(res2, "pram_params")$roof
+  expect_false(all(res1$roof_pram==testdata$roof&res1$walls_pram==testdata$walls&res1$water_pram==testdata$water))
 })
 
 test_that("pram on a data.frame, full matrix for pd", {
@@ -61,17 +57,15 @@ test_that("pram on a data.frame, full matrix for pd", {
   # rowSums() and colSums() must equal 1 too!
   mat <- diag(length(levels(testdata$roof)))
   rownames(mat) <- colnames(mat) <- levels(testdata$roof)
-  res3 <- pram(testdata,variables="roof", pd=mat)
-  print(res3) # of course, nothing has changed!
-  
+  res1 <- pram(testdata,variables="roof", pd=mat)
+  expect_true(all(res1$roof_pram==testdata$roof))
   ## it is possible use a transistion matrix for a variable and use the 'traditional' way
   ## of specifying a number for the minimal diagonal entries of the transision matrix
   ## for other variables. In this case we must supply \\code{pd} as list.
-  res4 <- pram(testdata,variables=c("roof","walls"), pd=list(mat,0.5), alpha=c(NA, 0.5))
-  print(res4)
-  summary(res4)
-  attr(res4, "pram_params")
+  res1 <- pram(testdata,variables=c("roof","walls"), pd=list(mat,0.5), alpha=c(NA, 0.5))
+  expect_false(all(res1$roof_pram==testdata$roof&res1$walls_pram==testdata$walls))
 })
+
 data(testdata2)
 testdata2$urbrur <- factor(testdata2$urbrur)
 test_that("pram on a sdcObj", {
@@ -81,7 +75,7 @@ test_that("pram on a sdcObj", {
                       keyVars=c('roof','walls','water','electcon','relat','sex'),
                       numVars=c('expend','income','savings'), w='sampling_weight')
   sdc <- pram(sdc, variables=c("urbrur"))
-  print(sdc, type="pram")
+  expect_true(is.data.table(sdc@pram$comparison$urbrur))
 })
 
 
@@ -93,7 +87,7 @@ test_that("pram on a sdcObj 2", {
                       numVars=c('expend','income','savings'), w='sampling_weight',
                       pramVars="urbrur")
   sdc <- pram(sdc)
-  print(sdc, type="pram")
+  expect_true(is.data.table(sdc@pram$comparison$urbrur))
 })
 
 test_that("pram on a sdcObj, matrix", {
@@ -105,8 +99,8 @@ test_that("pram on a sdcObj, matrix", {
   mat <- diag(length(levels(testdata2$roof)))
   rownames(mat) <- colnames(mat) <- levels(testdata2$roof)
   mat[1,] <- c(0.9,0,0,0.05,0.05)
-  expect_warning(sdc <- pram(sdc, variables="roof", pd=mat))
-  print(sdc, type="pram")
   # we can also have a look at the transitions
-  get.sdcMicroObj(sdc, "pram")$transitions
+  gsm <- get.sdcMicroObj(sdc, "pram")$transitions
+  expect_warning(sdc <- pram(sdc, variables="roof", pd=mat))
+  
 })
