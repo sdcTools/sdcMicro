bc13d4ec7fa8de73a7ef76e809ddd1bb0da970ab
alexkowa <alex@kowarik.net>
2019-07-29 12:27:24 +0200

    clean test_valTable

--- tests/testthat/test_valTable.R
+++ tests/testthat/test_valTable.R
@@ -1,13 +1,13 @@
-test_that("suda2 data frame",{
+test_that("valtable data frame",{
   data("Tarragona")
-  valTable(Tarragona[100:200,],
-    method=c("simple","onedims","pca","addNoise: additive"))
+  vT <- valTable(Tarragona[100:200,],
+    method=c("simple","onedims","pca"))#,"addNoise: additive"))
+  expect_true(is.data.frame(vT))
 })
 
 test_that("valtable more methods",{
   data("Tarragona")
-  valTable(Tarragona[100:200,],
-    method=c("simple","onedims","pca","clustpppca","mdav", "addNoise: additive", "swappNum"))
-
-
+  vT <- valTable(Tarragona[100:200,],
+    method=c("simple","onedims","pca","clustpppca"))##,"mdav"))##"swappNum"))# "addNoise: additive", ))
+  expect_true(is.data.frame(vT))
 })
