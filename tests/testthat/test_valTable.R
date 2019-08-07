485971a7e21e44cf847a3c8a66e7a79d57f0eb63
alexkowa <alex@kowarik.net>
2019-08-07 12:11:35 +0200

    expand tests for now working methods for valTable

--- tests/testthat/test_valTable.R
+++ tests/testthat/test_valTable.R
@@ -2,13 +2,15 @@ context("valTable")
 test_that("valTable data.frame",{
   data("Tarragona")
   vT <- valTable(Tarragona[100:200,],
-    method=c("simple","onedims","pca"))#,"addNoise: additive"))
+    method=c("simple","onedims","pca","addNoise:additive",
+             "addNoise:correlated","addNoise:correlated2","addNoise:restr",
+             "addNoise:outdect","addNoise:ROMM"))
   expect_true(is.data.frame(vT))
 })
 
 test_that("valtable more methods",{
   data("Tarragona")
   expect_warning(vT <- valTable(Tarragona[100:200,],
-    method=c("simple","onedims","pca","clustpppca")))##,"mdav"))##"swappNum"))# "addNoise: additive", ))
+    method=c("simple","onedims","pca","clustpppca","mdav","swappNum")))
   expect_true(is.data.frame(vT))
 })
