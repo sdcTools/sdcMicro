c3edb943c3a88047c13ad9a51c5c7458d74a6715
Bernhard Meindl <bernhard.meindl@statistik.gv.at>
2019-08-07 12:39:40 +0200

    cleanup; make `valTable()` a bit nicer

--- R/valTable.R
+++ R/valTable.R
@@ -2,11 +2,12 @@
 #'
 #' A Function for the comparison of different perturbation methods.
 #'
-#' Tabelarise the output from summary.micro. Will be enhanced to all
+#' Tabularize the output from [summary.micro()]. Will be enhanced to all
 #' perturbation methods in future versions.
 #'
-#' @param x data frame or matrix
-#' @param method microaggregation methods or adding noise methods or rank swapping.
+#' @param x a `data.frame` or a `matrix`
+#' @param method character vector defining names of microaggregation-, adding-noise
+#' or rank swapping methods.
 #' @param measure FUN for aggregation. Possible values are mean (default), median, trim, onestep.
 #' @param clustermethod clustermethod, if a method will need a clustering procedure
 #' @param aggr aggregation level (default=3)
@@ -15,29 +16,31 @@
 #' @param p Swapping range, if method swappNum has been chosen
 #' @param noise noise addition, if an addNoise method has been chosen
 #' @param w variables for swapping, if method swappNum has been chosen
-#' @param delta parameter for adding noise method \sQuote{correlated2}
+#' @param delta parameter for adding noise method `"correlated2"`
 #' @return Measures of information loss splitted for the comparison of different methods.
 #'
-#' Methods for adding noise should be named via \dQuote{addNoise: method}, e.g.
-#' \dQuote{addNoise: correlated}, i.e. the term \sQuote{at first} then followed
-#' by a \sQuote{:} and a blank and then followed by the name of the method as
-#' described in function \sQuote{addNoise}.
+#' @details Methods for adding noise should be named via `addNoise:{method}`, e.g.
+#' `addNoise:correlated`, where `{method}` specifies the desired method as
+#' described in [addNoise()].
 #' @author Matthias Templ
-#' @seealso \code{\link{microaggregation}}, \code{\link{summary.micro}}
-#' @references Templ, M. and Meindl, B., \emph{Software Development for SDC in
-#' R}, Lecture Notes in Computer Science, Privacy in Statistical Databases,
+#' @seealso [microaggregation()], [summary.micro()]
+#' @references Templ, M. and Meindl, B., `Software Development for SDC in
+#' R`, Lecture Notes in Computer Science, Privacy in Statistical Databases,
 #' vol. 4302, pp. 347-359, 2006.
 #' @keywords print
 #' @export
+#' @md
 #' @examples
-#'
 #' data(Tarragona)
 #' \dontrun{
-#' valTable(Tarragona[100:200,],
-#' method=c("simple","onedims","pca"))
-#' valTable(Tarragona,
-#' method=c("simple","onedims","pca","clustpppca",
-#' "mdav", "swappNum"))
+#' valTable(
+#'   x = Tarragona[100:200, ],
+#'   method=c("simple", "onedims", "pca"))
+#'
+#' valTable(
+#'   x = Tarragona,
+#'   method = c("simple", "onedims", "pca", "clustpppca", "mdav", "swappNum"))
+#'
 #' ## clustpppca in combination with Mclust outperforms
 #' ## the other algorithms for this data set...
 #' }
@@ -45,92 +48,44 @@ valTable <- function(x, method = c("simple", "onedims", "clustpppca", "addNoise:
   measure = "mean", clustermethod = "clara", aggr = 3, nc = 8, transf = "log",
   p = 15, noise = 15, w = 1:dim(x)[2], delta = 0.1) {
 
-  prcompRob <- function(X, k = 0, sca = "mad", scores = TRUE) {
-    ## Copyright: Croux and Filzmoser
-    n <- nrow(X)
-    p <- ncol(X)
-    if (k == 0) {
-      p1 <- min(n, p)
-    } else {
-      p1 <- k
-    }
-    S <- rep(1, p1)
-    V <- matrix(1:(p * p1), ncol = p1, nrow = p)
-    P <- diag(p)
-    m <- apply(X, 2, median)
-    Xcentr <- scale(X, center = m, scale = FALSE)
-    for (k in 1:p1) {
-      B <- Xcentr %*% P
-      Bnorm <- sqrt(apply(B^2, 1, sum))
-      A <- diag(1/Bnorm) %*% B
-      Y <- A %*% P %*% t(X)
-      if (sca == "mad")
-        s <- apply(Y, 1, mad)
-      # if (sca == 'tau') s <- apply(Y, 1, scale.tau) if (sca == 'A') s <- apply(Y, 1, scale.a)
-      j <- order(s)[n]
-      S[k] <- s[j]
-      V[, k] <- A[j, ]
-      if (V[1, k] < 0)
-        V[, k] <- (-1) * V[, k]
-      P <- P - (V[, k] %*% t(V[, k]))
-    }
-    if (scores) {
-      list(scale = S, loadings = V, scores = Xcentr %*% V)
-    } else list(scale = S, loadings = V)
-  }
+  # vars to keep from summary.micro()
+  keep <- c("amean", "amedian", "aonestep", "devvar", "amad", "acov", "acor",
+            "acors", "adlm", "apcaload", "apppcaload", "atotals", "pmtotals",
+            "util1", "deigenvalues", "risk0", "risk1", "risk2", "wrisk1", "wrisk2")
 
-  m <- list()
+  m <- out <- vector("list", length(method))
   for (i in 1:length(method)) {
-    print(paste("method", method[i], "will be applied"))
-    flush.console()
-    if (method[i] %in% c("simple", "single", "onedims", "pca", "pppca", "clustpca", "clustpppca",
-      "mdav", "influence", "rmd", "clustmcdpca", "mcdpca")) {
-      m[[i]] <- microaggregation(obj = x, method = method[i], measure = measure, clustermethod = clustermethod,
-        aggr = aggr, nc = nc, transf = transf)
+    message("method ", i, "|", length(method), ": ", shQuote(method[i]))
+    message("--> compute results")
+    if (method[i] %in% c("simple", "single", "onedims", "pca", "pppca", "clustpca",
+                         "clustpppca", "mdav", "influence", "rmd", "clustmcdpca", "mcdpca")) {
+      m[[i]] <- microaggregation(
+          obj = x,
+          method = method[i],
+          measure = measure,
+          clustermethod = clustermethod,
+          aggr = aggr,
+          nc = nc,
+          transf = transf)
     }
     if (method[i] == "swappNum") {
-      d <- rankSwap(x, P = p, K0 = NULL, R0 = NULL)
-      d <- list(x = x, mx = d)
-      class(d) <- "micro"
-      m[[i]] <- d
+      m[[i]] <- list(x = x, mx = rankSwap(x, P = p, R0 = NULL))
     }
     if (substring(method[i], 1, 8) == "addNoise") {
-      m[[i]] <- addNoise(x, noise = noise, method = substring(method[i], 10, nchar(method[i],
-        type = "width")))
-      m[[i]]$mx <- m[[i]]$xm
-      class(m[[i]]) <- "micro"
+      adn_meth <-  strsplit(method[i], ":")[[1]][2]
+      if (is.na(adn_meth)) {
+        stop(shQuote(method[i]), "is not a valid in argument `method`.", call. = FALSE)
+      }
+      m[[i]] <- list(x = x, mx = addNoise(obj = x, noise = noise, method = adn_meth)$xm)
     }
     if (method[i] == "dataGen") {
-      d <- dataGen(x, n = dim(x)[1])
-      d <- list(x = x, xm = d)
-      class(d) <- "micro"
-      m[[i]] <- d
+      m[[i]] <- list(x = x, xm = dataGen(x, n = dim(x)[1]))
     }
+    message("--> compute summary statistics")
+    out[[i]] <- as.data.frame(summary.micro(m[[i]])[keep])
   }
-  print("calculating summary statistics")
-  flush.console()
-  s <- list()
-  s <- lapply(m, summary, robCov = FALSE, robReg = FALSE)
-  g <- data.frame(amean = round(s[[1]][[3]], 3), amedian = round(s[[1]][[4]], 3), aonestep = round(s[[1]][[5]],
-    3), devvar = round(s[[1]][[6]], 3), amad = round(s[[1]][[7]], 3), acov = round(s[[1]][[8]],
-    3), acor = round(s[[1]][[10]], 3), acors = round(s[[1]][[12]], 3), adlm = round(s[[1]][[13]],
-    3), apcaload = round(s[[1]][[15]], 3), apppcaload = round(s[[1]][[16]], 3), atotals = round(s[[1]][[19]],
-    3), pmtotals = round(s[[1]][[20]], 3), util1 = round(s[[1]][[21]], 3), deigenvalues = round(s[[1]][[22]],
-    3), risk0 = round(s[[1]][[23]], 3), risk1 = round(s[[1]][[24]], 3), risk2 = round(s[[1]][[25]],
-    3), wrisk1 = round(s[[1]][[26]], 3), wrisk2 = round(s[[1]][[27]], 3))
-  if (length(s) > 1) {
-    for (i in 2:length(s)) {
-      g2 <- data.frame(amean = round(s[[i]][[3]], 3), amedian = round(s[[i]][[4]], 3),
-        aonestep = round(s[[i]][[5]], 3), devvar = round(s[[i]][[6]], 3), amad = round(s[[i]][[7]],
-          3), acov = round(s[[i]][[8]], 3), acor = round(s[[i]][[10]], 3), acors = round(s[[i]][[12]],
-          3), adlm = round(s[[i]][[13]], 3), apcaload = round(s[[i]][[15]], 3), apppcaload = round(s[[i]][[16]],
-          3), atotals = round(s[[i]][[19]], 3), pmtotals = round(s[[i]][[20]], 3),
-        util1 = round(s[[i]][[21]], 3), deigenvalues = round(s[[i]][[22]], 3), risk0 = round(s[[i]][[23]],
-          3), risk1 = round(s[[i]][[24]], 3), risk2 = round(s[[i]][[25]], 3), wrisk1 = round(s[[i]][[26]],
-          3), wrisk2 = round(s[[i]][[27]], 3))
-      g <- rbind(g, g2)
-    }
-  }
+  g <- do.call("rbind", out)
+  g <- apply(g, 2, function(x) round(x, digits = 3))
   g <- cbind(data.frame(method = method), g)
   g
 }
