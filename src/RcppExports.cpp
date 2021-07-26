0e4c5d2fcb5c645219e869b06ad9d0967804bfcc
matthias-da <templ@statistik.tuwien.ac.at>
2021-07-26 14:54:27 +0200

    new Rd files

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,11 +5,6 @@
 
 using namespace Rcpp;
 
-#ifdef RCPP_USE_GLOBAL_ROSTREAM
-Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
-Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
-#endif
-
 // cpp_calcSuppInds
 List cpp_calcSuppInds(NumericMatrix inp, NumericVector checkVals, List params);
 RcppExport SEXP _sdcMicro_cpp_calcSuppInds(SEXP inpSEXP, SEXP checkValsSEXP, SEXP paramsSEXP) {
