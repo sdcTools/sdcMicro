7f20e6a32ca720b1d1859fed537a1a5f5a1cf908
Bernhard Meindl <bernhard.meindl@gmail.com>
2022-01-20 15:36:11 +0000

    fix warning with shiny 1.7.x addressing #316

--- inst/shiny/sdcApp/ui.R
+++ inst/shiny/sdcApp/ui.R
@@ -1,12 +1,12 @@
 library(shiny)
 
 addResourcePath("sdcwww", file.path(
-  getShinyOption(".appDir", getwd()), 
+  getShinyOption(".appDir", getwd()),
   "www")
 )
 
 shinyUI(
-  navbarPage(id="mainnav", theme = paste0("sdcwww/", getShinyOption(".guitheme")), "sdcMicro GUI",
+  navbarPage(id="mainnav", theme = paste0("sdcwww/", getShinyOption(".guitheme")), title = "sdcMicro GUI",
     tabPanel("About/Help", uiOutput("ui_about")),
     tabPanel("Microdata", uiOutput("ui_inputdata")),
     tabPanel("Anonymize", uiOutput("ui_anonymize")),
@@ -14,7 +14,7 @@ shinyUI(
     tabPanel("Export Data", uiOutput("ui_export")),
     tabPanel("Reproducibility", uiOutput("ui_script")),
     tabPanel("Undo", uiOutput("ui_undo")),
-    tags$head(tags$script(
+    header = tags$head(tags$script(
       src = paste0("sdcwww/", getShinyOption(".guijsfile"))
     ))
   )
