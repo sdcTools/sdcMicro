fa855ceba9bd528bcc8a6b228ad55890e1dcc65b
Matthias Templ <matthias.templ@gmail.com>
2024-01-05 10:54:39 +0100

    new data

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -227,13 +227,13 @@ BEGIN_RCPP
 END_RCPP
 }
 
-RcppExport SEXP LocalRecProg_cpp(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
-RcppExport SEXP Mdav(SEXP, SEXP, SEXP, SEXP, SEXP);
-RcppExport SEXP measure_hierachical(SEXP);
-RcppExport SEXP measure_risk_cpp(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
-RcppExport SEXP measure_threshold(SEXP, SEXP);
-RcppExport SEXP RankSwap(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
-RcppExport SEXP Suda2(SEXP, SEXP, SEXP, SEXP, SEXP);
+RcppExport SEXP LocalRecProg_cpp(void *, void *, void *, void *, void *, void *, void *, void *, void *);
+RcppExport SEXP Mdav(void *, void *, void *, void *, void *);
+RcppExport SEXP RankSwap(void *, void *, void *, void *, void *, void *, void *, void *, void *);
+RcppExport SEXP Suda2(void *, void *, void *, void *, void *);
+RcppExport SEXP measure_hierachical(void *);
+RcppExport SEXP measure_risk_cpp(void *, void *, void *, void *, void *, void *);
+RcppExport SEXP measure_threshold(void *, void *);
 
 static const R_CallMethodDef CallEntries[] = {
     {"_sdcMicro_cpp_calcSuppInds", (DL_FUNC) &_sdcMicro_cpp_calcSuppInds, 3},
@@ -253,11 +253,11 @@ static const R_CallMethodDef CallEntries[] = {
     {"_sdcMicro_test_comparator", (DL_FUNC) &_sdcMicro_test_comparator, 5},
     {"LocalRecProg_cpp",    (DL_FUNC) &LocalRecProg_cpp,    9},
     {"Mdav",                (DL_FUNC) &Mdav,                5},
+    {"RankSwap",            (DL_FUNC) &RankSwap,            9},
+    {"Suda2",               (DL_FUNC) &Suda2,               5},
     {"measure_hierachical", (DL_FUNC) &measure_hierachical, 1},
     {"measure_risk_cpp",    (DL_FUNC) &measure_risk_cpp,    6},
     {"measure_threshold",   (DL_FUNC) &measure_threshold,   2},
-    {"RankSwap",            (DL_FUNC) &RankSwap,            9},
-    {"Suda2",               (DL_FUNC) &Suda2,               5},
     {NULL, NULL, 0}
 };
 
